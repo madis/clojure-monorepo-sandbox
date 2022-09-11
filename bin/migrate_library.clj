a4e959e47091a8b5ffa63bbfd06501a32bd1023e
Madis Nõmme <me@mad.is>
2022-09-11 11:26:34 +0000

    Specify sub-path(prefix) and document move-merging function

--- bin/migrate_library.clj
+++ bin/migrate_library.clj
@@ -16,12 +16,19 @@
         using-deps? (fs/exists? (str absolute-path "/deps.edn"))]
     (and using-deps?)))
 
-(defn move-merging-git-histories [source-path target-path]
-  (let [remote-name (str (last (fs/components source-path)))
+(defn move-merging-git-histories
+  "Creates branch and merges repository at source-path (with its commit
+  history) to repository at `target-path` using last component (folder) name as
+  the subfolder (or prefix) in the target. Optionally the destination can be
+  specified with 3rd argument, e.g. \"server/smart-contracts\". "
+  [source-path target-path & [subfolder-path]]
+  (let [source-name (str (last (fs/components source-path)))
+        prefix (or subfolder-path source-name)
         ; _ (sh "git" "reset" "--hard" :dir target-path) ; To avoid 'Working tree has modifications' error
         _ (sh "git" "stash" :dir target-path) ; Remember any changes in the working tree
+        _ (sh "git" "checkout" "-b" source-name)
         ; Idea from https://www.jvt.me/posts/2018/06/01/git-subtree-monorepo/
-        merge-result (sh "git" "subtree" "add" (str "--prefix=" remote-name) source-path "master" :dir target-path)
+        merge-result (sh "git" "subtree" "add" (str "--prefix=" source-name) source-path "master" :dir target-path)
         _ (sh "git" "stash" "pop" :dir target-path) ; Restore the changes in the working tree
         ]
     (if (= 1 (:exit merge-result))
